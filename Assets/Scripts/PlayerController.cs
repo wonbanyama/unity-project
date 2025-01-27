cd7c8be2fab83f7736a342f9068a4a4c433960e4
wonbanyama <parkminseok1970@gmail.com>
2025-01-28 00:21:58 +0900

    [init] 프로젝트 생성

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+    private float walkSpeed;
+
+    [SerializeField]
+    private Rigidbody myRigid;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
